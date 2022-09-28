d4c0f1c64136c96552e402a85c41c5a54d389193
Matthias <matthias-endler@gmx.net>
2022-09-28 12:33:48 +0200

    Serialze Cloudflare

--- forwarder/src/event.rs
+++ forwarder/src/event.rs
@@ -1,8 +1,10 @@
 use crate::client::client;
 use crate::helpers::upstream;
 use crate::{forward::extract_ref, posthog};
+use serde::ser::SerializeMap;
+use serde::{Serialize, Serializer};
 use serde_json::json;
-use worker::{Request, Result, RouteContext};
+use worker::{Cf, Request, Result as WorkerResult, RouteContext};
 
 fn request_kind(path: String) -> String {
     if path == "/" {
@@ -15,7 +17,7 @@ fn request_kind(path: String) -> String {
 }
 
 /// Create `PostHog` event from Cloudflare request
-pub(crate) fn posthog<D>(request: &Request, ctx: &RouteContext<D>) -> Result<posthog::Event> {
+pub fn posthog<D>(request: &Request, ctx: &RouteContext<D>) -> WorkerResult<posthog::Event> {
     let kind = request_kind(request.path());
     let mut event = posthog::Event::new(&kind, &upstream(ctx)?)
         .property("client", client(request).name())?
@@ -44,11 +46,28 @@ pub(crate) fn posthog<D>(request: &Request, ctx: &RouteContext<D>) -> Result<pos
     Ok(event)
 }
 
+struct Cloudflare<'a>(&'a Cf);
+
+impl<'a> Serialize for Cloudflare<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut map = serializer.serialize_map(Some(1))?;
+        map.serialize_entry("colo", &self.0.colo())?;
+        map.serialize_entry("asn", &self.0.asn())?;
+        map.serialize_entry("country", &self.0.country())?;
+        map.serialize_entry("http_protocol", &self.0.http_protocol())?;
+        map.serialize_entry("city", &self.0.city())?;
+        map.serialize_entry("continent", &self.0.continent())?;
+        map.serialize_entry("region", &self.0.region())?;
+        map.serialize_entry("region_code", &self.0.region_code())?;
+        map.end()
+    }
+}
+
 /// Create `OpenPodcast API` event from Cloudflare request
-pub(crate) fn openpodcast<D>(
-    request: &Request,
-    ctx: &RouteContext<D>,
-) -> Result<serde_json::Value> {
+pub fn openpodcast<D>(request: &Request, ctx: &RouteContext<D>) -> WorkerResult<serde_json::Value> {
     let (latitude, longitude) = request.cf().coordinates().unwrap_or((0.0, 0.0));
     // concatenate headers into a single string separated by semi-colons
     let headers = request
@@ -58,13 +77,16 @@ pub(crate) fn openpodcast<D>(
         .collect::<Vec<String>>()
         .join("; ");
 
+    // serialize `request.cf()` as json
+    let cloudflare = Cloudflare(request.cf());
+
     let event = json!({
         "kind":request_kind(request.path()),
         "upstream": upstream(ctx)?,
-        "upstream-ref": extract_ref(request).and_then(|s| Ok(s.to_string()))?,
+        "upstream-ref": extract_ref(request).map(|s| s.to_string())?,
         "client": client(request).name(),
         "is-bot": client(request).is_bot(),
-        "cloudflare": format!("{:#?}", request.cf()),
+        "cloudflare": cloudflare,
         "country": request.cf().country(),
         "path": request.path(),
         "latitude": latitude,
